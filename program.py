43e75cb798e0e1aaf801b530b6433f9fc23ae9a6
Thomas Cordeiro <thomcord@gmail.com>
2021-03-10 10:53:10 +0100

    first lines

---
+++ program.py
@@ -0,0 +1,13 @@
+# Simple project that will ask the user to guess a number.
+
+print('-----------------------')
+print('-----Guessing Game-----')
+print('-----------------------')
+print('In this game you have to try to guess a secret number from 0 to 20. You have 3 guesses. Good Luck!')
+
+secretNumber = 7
+numberOfTries = 3
+round = 1
+
+while (round <= nunmberofTries):
+    print(Try)
