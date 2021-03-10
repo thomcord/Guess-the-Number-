2b469cfc4aef1ceb1649deddf219765424a4c758
Thomas Cordeiro <thomcord@gmail.com>
2021-03-10 22:35:09 +0100

    complete code

--- program.py
+++ program.py
@@ -9,5 +9,23 @@ secretNumber = 7
 numberOfTries = 3
 round = 1
 
-while (round <= nunmberofTries):
-    print(Try)
+while (round <= numberOfTries):
+    print('Try {} from {}' .format(round, numberOfTries))
+    guess = int(input('Enter a numbe:'))
+    print('You entere:', guess)
+    
+    right = guess == secretNumber
+    bigger = guess > secretNumber
+    smaller = guess < secretNumber
+    
+    if (right):
+        print('Congratulations! You have entered the right number')
+        break
+    elif (bigger):
+        print('Wrong number! Your guess is higher then the secret number')
+    elif(smaller):
+        print('Wrong number! Your guess is lower then the secret number ')
+        
+        round = round +1
+        
+print('End of the game!')
